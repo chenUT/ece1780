0467ab26a816ada9e2e34aa8a0123727c9030c09
chen <tianclch@gmail.com>
2017-03-24 01:07:45 -0400

    update brain url

--- constants/constants.js
+++ constants/constants.js
@@ -3,9 +3,10 @@ const isDev = process.env.NODE_ENV !== 'production';
 let dbUrl = 'mongodb://localhost';
 let dbName = 'wondering_bot'
 
-let brainUrl = 'http://138.197.64.93:8080';
+let brainUrl = 'http://localhost:8080';
 if (!isDev) {
     dbUrl = 'mongodb://localhost';
+    brainUrl = 'http://138.197.64.93:8080';
     brainUrl = 'http://localhost:8080';
 }
 
